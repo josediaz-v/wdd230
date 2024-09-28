33d9665a0d63377aaf0645fccee08b71e5c87f0f
josediaz-v <josediaz_v@hotmail.com>
2024-09-28 02:11:19 -0400

    W02: Team Activity - Home Page Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,26 @@ let lastModification = document.lastModified;
 
 document.querySelector('#year').textContent = currentYear.getFullYear();
 document.querySelector('#lastModified').textContent = `Last Modification: ${lastModification}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
