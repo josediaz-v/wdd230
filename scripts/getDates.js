481d7a03934c52eb590e64636b5323ca128f6ce3
Jose Diaz <dia20013@byui.edu>
2024-10-03 23:01:39 -0400

    Update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,34 +3,3 @@ let lastModification = document.lastModified;
 
 document.querySelector('#year').textContent = currentYear.getFullYear();
 document.querySelector('#lastModified').textContent = `Last Modification: ${lastModification}`;
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
-
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-const links = document.querySelectorAll("#link");
-
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "🔆";
-		links.forEach(link => {
-		link.style.color = "#fff";
-		});
-	}
-	else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "🕶️";
-		links.forEach(link => {
-		link.style.color = "#154A82";
-		});
-	}
-});
---
+++ scripts/modeButton.js
@@ -0,0 +1,26 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const links = document.querySelectorAll("#link");
+const body = document.querySelector("body");
+//const map = document.querySelector("iframe");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+		links.forEach(link => {
+		link.style.color = "#fff";
+        body.style.background = "#000";
+		});
+	}
+	else {
+        body.style.background = "#fff";
+		main.style.background = "#fff";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+		links.forEach(link => {
+		link.style.color = "#154A82";
+		});
+	}
+});
